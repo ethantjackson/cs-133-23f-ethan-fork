974a1d66714f9769772e806cd0ece9db846485c4
Ubuntu <ubuntu@ip-172-31-33-100.us-east-2.compute.internal>
2023-10-29 04:13:38 +0000

    Fix MPI scattering and broadcasting

--- lab2/mpi.cpp
+++ lab2/mpi.cpp
@@ -13,58 +13,56 @@
 void GemmParallelBlocked(const float a[kI][kK], const float b[kK][kJ],
                          float c[kI][kJ])
 {
-  float *a_contiguous = (float *)lab2::aligned_alloc(1024, kI * kK * sizeof(float));
-  float *b_contiguous = (float *)lab2::aligned_alloc(1024, kK * kJ * sizeof(float));
-  float *c_contiguous = (float *)lab2::aligned_alloc(1024, kI * kJ * sizeof(float));
   int rank;
   MPI_Comm_rank(MPI_COMM_WORLD, &rank);
+  int num_processes;
+  MPI_Comm_size(MPI_COMM_WORLD, &num_processes);
+  int start = rank * kI / num_processes;
+  int end = (rank + 1) * kI / num_processes;
+  float *a_contiguous = (float*)lab2::aligned_alloc(64, kI*kK * sizeof(float));
+  float *b_contiguous = (float*)lab2::aligned_alloc(64, kK*kJ * sizeof(float));
+  for (int i = 0; i < kK * kJ; ++i) 
+    b_contiguous[i] = 0;
+
   if (rank == 0)
   {
     for (int i = 0; i < kI; ++i)
     {
       for (int j = 0; j < kK; ++j)
-        a_contiguous[kK * i + j] = a[i][j];
+        a_contiguous[i * kK + j] = a[i][j];
     }
     for (int i = 0; i < kK; ++i)
     {
       for (int j = 0; j < kJ; ++j)
-        b_contiguous[kJ * i + j] = b[i][j];
-    }
-    for (int i = 0; i < kI; ++i)
-    {
-      for (int j = 0; j < kJ; ++j)
-        c_contiguous[kJ * i + j] = c[i][j];
+        b_contiguous[i * kJ + j] = b[i][j];
     }
   }
-  MPI_Barrier(MPI_COMM_WORLD);
+  float *a_local = (float*)lab2::aligned_alloc(64, kI*kK / num_processes * sizeof(float));
+  float *c_local = (float*)lab2::aligned_alloc(64, kI*kJ / num_processes * sizeof(float));
 
-  int size;
-  MPI_Comm_size(MPI_COMM_WORLD, &size);
-  int chunk_size = kI / size;
-  float *rec_A = (float *)lab2::aligned_alloc(1024, chunk_size * kK * sizeof(float));
-  float *rec_C = (float *)lab2::aligned_alloc(1024, chunk_size * kJ * sizeof(float));
-  MPI_Scatter(a_contiguous, chunk_size * kK, MPI_FLOAT, rec_A, chunk_size * kK, MPI_FLOAT, 0, MPI_COMM_WORLD);
+  float *c_contiguous = (float*)lab2::aligned_alloc(64, kI*kJ * sizeof(float));
+  printf("send_amount: %d\n", kI * kK / num_processes);
+
+  MPI_Scatter(a_contiguous, kI * kK / num_processes, MPI_FLOAT, a_local, kI * kK / num_processes, MPI_FLOAT, 0, MPI_COMM_WORLD);
   MPI_Bcast(b_contiguous, kK * kJ, MPI_FLOAT, 0, MPI_COMM_WORLD);
-  for (int ii = 0; ii < chunk_size; ii += 64)
+  printf("computing slice %d (starting at %d to %d)\n", rank, start, end - 1);
+  for (int ii = 0; ii < kI/num_processes; ii += 64)
     for (int kk = 0; kk < kK; kk += 8)
       for (int jj = 0; jj < kJ; jj += 1024)
         for (int i = ii; i < ii + 64; ++i)
           for (int j = jj; j < jj + 1024; ++j)
           {
-            float reg = 0;
             for (int k = kk; k < kk + 8; ++k)
-              reg += rec_A[i * kK + k] * b_contiguous[k * kJ + j];
-            rec_C[i * kJ + j] = reg;
+              c_local[i*kJ +j] += a_local[i * kK + k] * b_contiguous[k * kJ + j];
           }
+  
+  MPI_Gather(c_local, kI * kJ / num_processes, MPI_FLOAT, c_contiguous, kI*kJ / num_processes, MPI_FLOAT, 0, MPI_COMM_WORLD);
 
-  MPI_Gather(rec_C, chunk_size * kJ, MPI_FLOAT, c_contiguous, chunk_size * kJ, MPI_FLOAT, 0, MPI_COMM_WORLD);
-
-  if (rank == 0)
-  {
-    for (int i = 0; i < kI; ++i)
-    {
-      for (int j = 0; j < kJ; ++j)
-        c[i][j] = c_contiguous[kJ * i + j];
+  if (rank == 0) {
+    for (int i = 0; i < kI; ++i) {
+      for (int j = 0; j < kJ; ++j) {
+        c[i][j] = c_contiguous[i * kJ + j];
+      }
     }
   }
 }
